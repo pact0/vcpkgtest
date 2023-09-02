29b2f5ba32f4b6623ea25d73a250dd5af788e161
pact0 <kuba.kowalski21331@gmail.com>
2023-09-02 22:08:54 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,20 @@
 #include <cstdio>
 // #include <fmt/core.h>
 #include <spdlog/spdlog.h>
-#include <QApplication>
-#include <QMainWindow>
+// #include <QApplication>
+// #include <QMainWindow>
 
 // main function!
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    fmt::print("Hello, world!\n");
-    printf("%s\n", sqlite3_libversion());
-    // spdlog::warn("A");
+    return 0;
+    // QApplication app(argc, argv);
+    // fmt::print("Hello, world!\n");
+    // printf("%s\n", sqlite3_libversion());
+    // // spdlog::warn("A");
 
-    QMainWindow mainWindow;
-    mainWindow.setWindowTitle("Moje Okno");
-    mainWindow.show();
-    return app.exec();
+    // QMainWindow mainWindow;
+    // mainWindow.setWindowTitle("Moje Okno");
+    // mainWindow.show();
+    // return app.exec();
 }
