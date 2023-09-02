c900126dcfcb2cdf8fa6cea389634efa8d2bdc4f
pact0 <kuba.kowalski21331@gmail.com>
2023-09-02 17:10:29 +0200

    LL

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 // main.cpp
+#define FMT_HEADER_ONLY
 #include <sqlite3.h>
 #include <cstdio>
-#include <fmt/core.h>
+// #include <fmt/core.h>
+#include <spdlog/spdlog.h>
 
 // main function!
 int main()
 {
     fmt::print("Hello, world!\n");
     printf("%s\n", sqlite3_libversion());
-    return 0;
+    spdlog::warn("A") return 0;
 }
