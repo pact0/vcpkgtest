0796a700da812151d883ac7b0be046e9f4b701ea
pact0 <kuba.kowalski21331@gmail.com>
2023-09-02 16:52:04 +0200

    AB

--- src/test.cpp
+++ src/test.cpp
@@ -2,11 +2,13 @@
 #include <sqlite3.h>
 #include <stdio.h>
 #include <fmt/core.h>
+#include <spdlog/spdlog.h>
 
 // main function!
 int main()
 {
     fmt::print("test!\n");
+    spdlog::warn("A");
     printf("%s\n", sqlite3_libversion());
     return 0;
 }
